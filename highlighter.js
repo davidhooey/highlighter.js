a019a7d84769c310c51676c36cbff77fe20510e3
David Hooey <davidhooey@gmail.com>
2019-02-01 13:22:59 -0500

    Fixing Issue #1 - Fails to highlight the letter I within a <i></i> tag.

--- highlighter.js
+++ highlighter.js
@@ -1,9 +1,9 @@
 (function( $ ) {
 	$.fn.highlighter = function(options) {
 		// this is a jQuery object. No need to do $(this)
-		
+
 		// Options
-		// 
+		//
 		// $('div').highlighter({
 		//  'class' : 'highlighter',
 		//  'terms' : searchTerms
@@ -12,7 +12,7 @@
 	    	'class' : 'highlight',
 	    	'terms' : []
 	    }, options);
-	    
+
 		// Returns the jQuery object for chainablity
 		return this.each(function() {
 			if (settings.terms.length > 0) {
@@ -20,25 +20,42 @@
 				var cjk_range2 = "[\u3400-\u4DBF]";
 				var data = $(this).html();
 				var boundary_chars = " \n\r\t.,“”<>\–\"\+!&;:?-";
-								
-				$.each(settings.terms, function(term) {
+
+				$.each(settings.terms, function(i, term) {
 					// Required RegEx for Russian and likely all non-ASCII characters.
 					// (^\|[ \n\r\t.,<>'\"\+!?-]+)(faith[^ \n\r\t.,<>'\"\+!?-]*)([ \n\r\t.,<>'\"\+!?-]+\|$)
-					if (settings.terms[term].search(/^\*(.+)\*$/) != -1) {
-						data = data.replace(RegExp("(^\|[" + boundary_chars + "]+)([^" + boundary_chars + "]*" + settings.terms[term].replace(/\*/g,'') + "[^" + boundary_chars + "]*)([" + boundary_chars + "]+\|$)",'gi'), "$1<span class='" + settings.class + "'>$2</span>$3", 'gi');
+
+					// Special handle for the word 'I' as it also matches <i> tags.
+					if (term.toLowerCase() === "i") {
+						var word_split = data.split(" ");
+						$.each(word_split, function(w, word) {
+							if (word.toLowerCase() === '<i>i') {
+								word_split[w] = "<i><span class='" + settings.class + "'>I</span>";
+							}
+							else if (word.toLowerCase() === 'i</i>') {
+								word_split[w] = "<span class='" + settings.class + "'>I</span></i>";
+							}
+							else {
+								word_split[w] = word.replace(RegExp("(^\|[" + boundary_chars + "]+)(" + term + ")([" + boundary_chars + "]+\|$)", 'gi'), "$1<span class='" + settings.class + "'>$2</span>$3", 'gi');
+							}
+						});
+						data = word_split.join(" ");
 					}
-					else if (settings.terms[term].search(/^\*(.+)$/) != -1) {
-						data = data.replace(RegExp("(^\|[" + boundary_chars + "]+)([^" + boundary_chars + "]*" + settings.terms[term].replace(/\*/g,'') + ")([" + boundary_chars + "]+\|$)",'gi'), "$1<span class='" + settings.class + "'>$2</span>$3", 'gi');
-					}			
-					else if (settings.terms[term].search(/^(.+)\*$/) != -1) {
-						data = data.replace(RegExp("(^\|[" + boundary_chars + "]+)(" + settings.terms[term].replace(/\*/g,'') + "[^" + boundary_chars + "]*)([" + boundary_chars + "]+\|$)",'gi'), "$1<span class='" + settings.class + "'>$2</span>$3", 'gi');
+					else if (term.search(/^\*(.+)\*$/) != -1) {
+						data = data.replace(RegExp("(^\|[" + boundary_chars + "]+)([^" + boundary_chars + "]*" + term.replace(/\*/g,'') + "[^" + boundary_chars + "]*)([" + boundary_chars + "]+\|$)",'gi'), "$1<span class='" + settings.class + "'>$2</span>$3", 'gi');
 					}
-					else if (settings.terms[term].search(/^(.+)$/) != -1) {
-						if (RegExp("(" + cjk_range1 + ")|(" + cjk_range2 + ")").test(settings.terms[term])) {
-							data = data.replace(settings.terms[term], "<span class='" + settings.class + "'>" + settings.terms[term] + "</span>", 'gi');
+					else if (term.search(/^\*(.+)$/) != -1) {
+						data = data.replace(RegExp("(^\|[" + boundary_chars + "]+)([^" + boundary_chars + "]*" + term.replace(/\*/g,'') + ")([" + boundary_chars + "]+\|$)",'gi'), "$1<span class='" + settings.class + "'>$2</span>$3", 'gi');
+					}
+					else if (term.search(/^(.+)\*$/) != -1) {
+						data = data.replace(RegExp("(^\|[" + boundary_chars + "]+)(" + term.replace(/\*/g,'') + "[^" + boundary_chars + "]*)([" + boundary_chars + "]+\|$)",'gi'), "$1<span class='" + settings.class + "'>$2</span>$3", 'gi');
+					}
+					else if (term.search(/^(.+)$/) != -1) {
+						if (RegExp("(" + cjk_range1 + ")|(" + cjk_range2 + ")").test(term)) {
+							data = data.replace(term, "<span class='" + settings.class + "'>" + term + "</span>", 'gi');
 						}
 						else {
-							data = data.replace(RegExp("(^\|[" + boundary_chars + "]+)(" + settings.terms[term] + ")([" + boundary_chars + "]+\|$)",'gi'), "$1<span class='" + settings.class + "'>$2</span>$3", 'gi');
+							data = data.replace(RegExp("(^\|[" + boundary_chars + "]+)(" + term + ")([" + boundary_chars + "]+\|$)",'gi'), "$1<span class='" + settings.class + "'>$2</span>$3", 'gi');
 						}
 					}
 				});
